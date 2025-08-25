4abb78ed8d47012112bb46ae2ae3f9c5bfb36336
cj <jb.carl@outlook.com>
2025-08-24 18:21:16 -0600

    Added: Core financial support models

--- financeTracker/asset/models.py
+++ financeTracker/asset/models.py
@@ -67,3 +67,139 @@ class CreditAccount(BaseAccount):
         return f"{self.account_holder}'s {self.name} @ {self.currency.symbol}{self.balance} {self.currency.code}"
     
 
+
+
+# --------------------------
+# Financial Supporting Models
+# --------------------------
+
+class StockExchange(models.Model):
+    name = models.CharField(max_length=100)            # e.g., "New York Stock Exchange"
+    code = models.CharField(max_length=10, unique=True) # e.g., "NYSE" or "XNYS"
+    country = models.CharField(max_length=50)          # e.g., "USA"
+    timezone = models.CharField(max_length=50, null=True)        # e.g., "America/New_York"
+    
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+    
+
+class Stock(models.Model):
+    symbol = models.CharField(max_length=10, unique=True)  
+    name = models.CharField(max_length=100)
+    exchange = models.ForeignKey(StockExchange, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, on_delete=models.PROTECT)
+    unite_price = models.DecimalField(max_digits=20, decimal_places=4, default=Decimal("0.00"))
+
+    def __str__(self):
+        return f"{self.symbol} ({self.exchange.code})"
+    
+
+
+class Commodity(models.Model):
+    symbol = models.CharField(max_length=10)  
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.symbol} ({self.name})"
+
+class BrokerageAccount(BaseAccount):
+    """Generic brokerage account."""
+    balance = models.DecimalField(max_digits=20, decimal_places=4, default=Decimal("0"))
+    #stock_holdings = models.ManyToManyField(Stock, through='StockHolding', related_name='brokerage_accounts')
+
+    def __str__(self):
+        return f"{self.account_holder}'s {self.name} (Brokerage) - {self.currency.code}"
+
+
+class TFSA(BrokerageAccount):
+    def __str__(self):
+        return f"{self.account_holder} TFSA Account {self.currency.symbol}{self.currency.code}" 
+
+
+class StockHolding(models.Model):
+    account = models.ForeignKey(BrokerageAccount, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=20, decimal_places=8, default=Decimal("0.00"))   # fractional shares
+    average_price = models.DecimalField(max_digits=20, decimal_places=4, default=Decimal("0.00"))  # avg cost/share
+    date = models.DateTimeField(auto_now_add=True) 
+
+    class Meta:
+        unique_together = ('account', 'stock')  # Ensure each stock is unique per account
+
+    def __str__(self):
+        return f"{self.quantity} of {self.stock.name} in {self.account.account_holder}'s TFSA"
+    
+
+
+    def buy(self, quantity, price):
+        """Buy stock: increase quantity, update avg price, reduce portfolio balance"""
+        total_cost = quantity * price
+
+
+        # Check available balance
+        if self.account.balance < total_cost:
+            raise ValueError("Insufficient funds")
+        #deduct cash
+        self.account.balance -= total_cost
+        self.account.save()
+
+        # Weighted average price calculation
+        current_total_cost = self.quantity * self.average_price
+        new_total_shares = self.quantity + quantity
+        new_total_cost = current_total_cost + total_cost
+
+        if new_total_shares > 0:
+            self.average_price = new_total_cost / new_total_shares
+        #update shares
+        self.quantity = new_total_shares
+        self.save()
+
+        # record Transaction
+        Transaction.objects.create(
+            holding = self,
+            transaction_type = "BUY",
+            quantity = quantity,
+            price = price
+        )
+
+
+    def sell(self, quantity, price):
+        """Sell stock: decrease quantity, increase portfolio balance"""
+        if self.quantity < quantity:
+            raise ValueError("You have less share available than you are trying to sell")
+        
+        total_proceeds = quantity * price
+
+        #reduce shares
+        self.quantity -= quantity
+        self.save()
+
+        #add cash
+        self.account.balance += total_proceeds
+        self.account.save()
+
+        #record Transaction
+        Transaction.objects.create(
+            holding=self,
+            transaction_type = "SELL",
+            quantity=quantity,
+            price=price
+        )
+
+
+class Transaction(models.Model):
+    holding = models.ForeignKey(StockHolding, on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=4, choices=[('BUY', 'Buy'), ('SELL', 'Sell')])
+    quantity = models.DecimalField(max_digits=12, decimal_places=6)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+        
+
+
+
+
+
+
+
+
+
